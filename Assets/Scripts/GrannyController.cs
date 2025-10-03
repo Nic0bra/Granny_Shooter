4459844d60ac6d07cd0808eb2bba7f65ce2d8085
Nicole <x0obubbleso0x@gmail.com>
2025-10-03 10:51:45 -0500

    update granny controller

--- Assets/Scripts/GrannyController.cs
+++ Assets/Scripts/GrannyController.cs
@@ -2,15 +2,45 @@ using UnityEngine;
 
 public class GrannyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    Granny_Input _inputActions;
+    [Header("Movement Variables")]
+    [SerializeField] float movespeed = 5f;
+    [SerializeField] float jumpForce = 12f;
+    [SerializeField] Vector2 moveInput;
+    [SerializeField] Rigidbody _rb;
+    private void Awake()
     {
-        
+        _inputActions = new Granny_Input();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+    private void OnDisable()
+    {
+        _inputActions.Disable();
+    }
+    private void Update()
+    {
+        moveInput = _inputActions.Player.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+
+        _rb.MovePosition(transform.position + moveDirection * movespeed * Time.fixedDeltaTime);
     }
 }
