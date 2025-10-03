22611ba9b7761c10d6c9f592e113cd662e6d7843
Nicole <x0obubbleso0x@gmail.com>
2025-10-03 12:04:58 -0500

    did some stuff

--- Assets/Scripts/GrannyController.cs
+++ Assets/Scripts/GrannyController.cs
@@ -1,14 +1,20 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class GrannyController : MonoBehaviour
 {
 
     Granny_Input _inputActions;
-    [Header("Movement Variables")]
-    [SerializeField] float movespeed = 5f;
-    [SerializeField] float jumpForce = 12f;
-    [SerializeField] Vector2 moveInput;
     [SerializeField] Rigidbody _rb;
+    private Transform _cam;
+
+    [Header("Movement Variables")]
+    public float movespeed = 5f;
+    public float jumpForce = 12f;
+    public float turnSmoothing = 0.25f;
+    public float turnSmoothVelocity;
+    [SerializeField] Vector2 moveInput;
+    
     private void Awake()
     {
         _inputActions = new Granny_Input();
@@ -40,7 +46,22 @@ public class GrannyController : MonoBehaviour
     void PlayerMove()
     {
         Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        
+        if(moveDirection != Vector3.zero)
+        {
+            if(moveDirection.magnitude >= .1)
+            {
+                float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + _cam.eulerAngles.y;
+                float _angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, 
+                                                     targetAngle, 
+                                                     ref turnSmoothVelocity, 
+                                                     turnSmoothing);
 
-        _rb.MovePosition(transform.position + moveDirection * movespeed * Time.fixedDeltaTime);
+                transform.rotation = Quaternion.Euler(0, _angle, 0);
+
+                Vector3 moveDirCam = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+                _rb.MovePosition(transform.position + moveDirection * movespeed * Time.fixedDeltaTime);
+            }
+        } 
     }
 }
